91904b1f08b9d762291d1897a2081689c85496f9
Maushimo <Maushimo@users.noreply.github.com>
2018-12-15 14:22:04 +0000

    finished chapter 6

--- app/models.py
+++ app/models.py
@@ -3,6 +3,7 @@ from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from app import login
+from hashlib import md5
 
 #allows flask_login to load a user via user_id
 @login.user_loader
@@ -17,6 +18,8 @@ class User(UserMixin, db.Model):
 	email = db.Column(db.String(120), index=True, unique=True)
 	password_hash = db.Column(db.String(120))
 	posts = db.relationship('Post', backref='author', lazy='dynamic')
+	about_me = db.Column(db.String(140))
+	last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
 	#magic "reprint" function
 	def __repr__(self):
@@ -27,6 +30,10 @@ class User(UserMixin, db.Model):
 
 	def check_password(self, password):
 		return check_password_hash(self.password_hash, password)
+
+	def avatar(self, size):
+		digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+		return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
 
 class Post(db.Model):
 	#table containing blogpost information
