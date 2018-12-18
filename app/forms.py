d95bd2af6fe71212fecac5a74348ac97ab415c26
Maushimo <Maushimo@users.noreply.github.com>
2018-12-18 14:43:28 +0000

    chapter 7 complete

--- app/forms.py
+++ app/forms.py
@@ -31,3 +31,13 @@ class EditProfileForm(FlaskForm):
 	username = StringField('User', validators=[DataRequired()])
 	about_me = TextAreaField('About Me', validators=[Length(min=0, max=140)])
 	submit = SubmitField('Submit')
+
+	def __init__(self, original_username, *arg, **kwargs):
+		super(EditProfileForm, self).__init__(*arg, **kwargs)
+		self.original_username = original_username
+
+	def validate_username(self, username):
+		if username.data != self.original_username:
+			user = User.query.filter_by(username=self.username.data).first()
+			if user is not None:
+				raise ValidationError('Please use a different username')
